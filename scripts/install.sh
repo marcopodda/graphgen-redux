df1f4c10049cb1c612973c667d6f8953df189ff1
Marco Podda <marco.podda@di.unipi.it>
2021-01-13 00:42:15 +0100

    fixed install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,33 +1,38 @@
 # RUN WITH SOURCE, not BASH!!!
 
-TORCH_VERSION=1.6.0
-CUDA_VERSION=cu102
+TORCH_VERSION=1.7.0
+CUDA_VERSION=cu110
 PYTHON_VERSION=3.7
 ENV_NAME=rgg
 
 # create venv
 conda create --name ${ENV_NAME} python=${PYTHON_VERSION} -y && conda activate ${ENV_NAME}
 
+# install base packages
+conda install scikit-learn pandas joblib networkx pyyaml seaborn ipython jupyter libboost -y
+
 # install pytorch
 conda install pytorch==${TORCH_VERSION} -c pytorch -y
 
-# install pytorch-geometric
-pip install torch-scatter==latest+${CUDA_VERSION} -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}.html
-pip install torch-sparse==latest+${CUDA_VERSION} -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}.html
-pip install torch-cluster==latest+${CUDA_VERSION} -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}.html
-pip install torch-spline-conv==latest+${CUDA_VERSION} -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}.html
+# install pytorch-geometr:ic
+pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
+pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
+pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
+pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH_VERSION}+${CUDA_VERSION}.html
 pip install torch-geometric
 
 # additional pip packages
-pip install networkx numpy scikit-learn scipy pyemd pytorch-lightning tqdm dgl-${CUDA_VERSION}
+pip install pyemd dgl-${CUDA_VERSION}
+pip install pytorch-lightning
 
 # instal EdEN (for NSPKD)
 pip install git+https://github.com/fabriziocosta/EDeN.git
 
 # compile cpps
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$LD_LIBRARY_PATH
 mkdir -p bin
 
 g++ -std=c++11 dfscode/dfs_code.cpp -o bin/dfscode -O3
 g++ -std=c++11 metrics/orca/orca.cpp -o bin/orca -O3
-g++ -std=c++11 metrics/isomorph.cpp -O3 -o bin/subiso -fopenmp -I$HOME/boost/include
-g++ -std=c++11 metrics/unique.cpp -O3 -o bin/unique -fopenmp -I$HOME/boost/include
+g++ -std=c++11 metrics/isomorph.cpp -O3 -o bin/subiso -fopenmp -I$CONDA_PREFIX/include
+g++ -std=c++11 metrics/unique.cpp -O3 -o bin/unique -fopenmp -I$CONDA_PREFIX/include
