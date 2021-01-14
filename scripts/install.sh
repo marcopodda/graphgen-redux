8419d0ef29286bc328bb3d7266ead0ea1dfab978
Marco Podda <marco.podda@di.unipi.it>
2021-01-15 00:08:29 +0100

    updated install script torch 1.6.0 for bug in 1.7.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 # RUN WITH SOURCE, not BASH!!!
 
-TORCH_VERSION=1.7.0
-CUDA_VERSION=cu110
+TORCH_VERSION=1.6.0
+CUDA_VERSION=cu102
 PYTHON_VERSION=3.7
-ENV_NAME=rgg
+ENV_NAME=gen
 
 # create venv
 conda create --name ${ENV_NAME} python=${PYTHON_VERSION} -y && conda activate ${ENV_NAME}
@@ -32,7 +32,7 @@ pip install git+https://github.com/fabriziocosta/EDeN.git
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$LD_LIBRARY_PATH
 mkdir -p bin
 
-g++ -std=c++11 dfscode/dfs_code.cpp -o bin/dfscode -O3
+g++ -std=c++11 datasets/dfs_code.cpp -o bin/dfscode -O3
 g++ -std=c++11 metrics/orca/orca.cpp -o bin/orca -O3
 g++ -std=c++11 metrics/isomorph.cpp -O3 -o bin/subiso -fopenmp -I$CONDA_PREFIX/include
 g++ -std=c++11 metrics/unique.cpp -O3 -o bin/unique -fopenmp -I$CONDA_PREFIX/include
