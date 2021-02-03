049c24f5efa2f7e5ed9c1c6d1103f0be54c633f2
Marco Podda <marco.podda@di.unipi.it>
2021-02-03 01:12:43 +0100

    ok

---
+++ analysis/utils.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from core.serialization import load_pickle
+
+RESULTS_DIR = Path("RESULTS")
+
+MODELS = [
+    "graphgen-redux",
+    "graphgen-full",
+    "graphgen-lw"
+]
+
+def load_results_on_dataset(dataset_name, last=False):
+    dataset_dir = RESULTS_DIR / dataset_name
+
+    dataset_results = {
+        "Validity": [],
+        "Novelty": [],
+        "Uniqueness": [],
+        "Node count avg. ref": [],
+        "Node count avg. pred": [],
+        "Edge count avg. ref": [],
+        "Edge count avg. pred": [],
+        "MMD Degree": [],
+        "MMD Clustering": [],
+        "MMD Orbit": [],
+        "MMD NSPDK": [],
+        "MMD Node labels": [],
+        "MMD Edge labels": [],
+        "MMD Node labels and degrees": []
+    }
+
+    for model_name in MODELS:
+        exp_dir = dataset_dir / model_name
+        eval_dir = exp_dir / "evaluation"
+        idx = 1 if last else 0
+        results_path = sorted(eval_dir.glob("*.pkl"))[idx]
+        results = load_pickle(results_path)
+        for key in results:
+            dataset_results[key].append(results[key])
+
+    return dataset_results
